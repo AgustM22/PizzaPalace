081c77151370fe7da8dc6215c453fe027e47fbb4
AgustM22 <agust22@ru.is>
2023-05-01 17:25:47 +0000

    merge

--- PizzaPalace/pizza/static/js/index.js
+++ PizzaPalace/pizza/static/js/index.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+const Menu = () => {
+    const Main = document.getElementById("MainBlock")
+    Main.innerHTML = ""
+
+    const SearchBar = document.createElement("div")
+    const Search = document.createElement("input")
+    const PizzaText = document.createElement("p")
+    const Sliders = document.createElement("img")
+
+    SearchBar.setAttribute("id", "SearchBar")
+    Search.setAttribute("placeholder", "Search...")
+    PizzaText.innerText = "Pizza"
+    Sliders.setAttribute("alt", "Settings")
+    Sliders.setAttribute("src", "/static/images/sliders.svg")
+
+    SearchBar.appendChild(Search)
+    SearchBar.appendChild(PizzaText)
+    SearchBar.appendChild(Sliders)
+    Main.appendChild(SearchBar)
+}
+
+Menu()
