d22bb70f66b9c76035c231da3980a5786aec3bdf
HermannHelgi <hermannt22@ru.is>
2023-05-02 11:02:09 +0000

    fixed errors in with merging

--- PizzaPalace/pizza/views.py
+++ PizzaPalace/pizza/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
 def main(request):
-<<<<<<< HEAD
     return render(request, "Homepage.html")
-=======
-    return render(request, "menu/menu.html")
->>>>>>> 62114d8bca20947bf4a2a0500d94244738b768f3
