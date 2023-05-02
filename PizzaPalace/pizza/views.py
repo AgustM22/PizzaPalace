587f7c36fcd7c2675d6e086775f554eeeacabe51
AgustM22 <agust22@ru.is>
2023-05-02 12:07:00 +0000

    editing menu page

--- PizzaPalace/pizza/views.py
+++ PizzaPalace/pizza/views.py
@@ -41,8 +41,8 @@ seed = [
         }
     },
     {
-        "name": "Margharita",
-        "ingredients": "Cheese, Sauce",
+        "name": "Hawaiian",
+        "ingredients": "Cheese, Sauce, pineapple",
         "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
         "tags": [
             "vegetarian"
@@ -55,7 +55,7 @@ seed = [
     },
     {
         "name": "Clayo",
-        "ingredients": "Clay and Mayo",
+        "ingredients": "Clay, Mayo",
         "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
         "tags": [
             "vegetarian"
