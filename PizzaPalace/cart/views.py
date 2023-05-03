f4224744fc4f801e3b14818e80146c9c663535fd
HermannHelgi <hermannt22@ru.is>
2023-05-03 15:47:13 +0000

    Made the base for cart

--- PizzaPalace/cart/views.py
+++ PizzaPalace/cart/views.py
@@ -1,4 +1,138 @@
 from django.shortcuts import render
 
+cart = [
+    {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+    {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+        {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": "1.000.kr",
+        "quantity": "1"
+    },
+]
+
 def main(request):
-    return render(request, "CartBase.html")
+    return render(request, "CartBase.html",context= {"cart":cart})
