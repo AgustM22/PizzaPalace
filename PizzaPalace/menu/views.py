15f68a3cb5d3d3410583457178f9e5d3e3eb75a6
AgustM22 <agust22@ru.is>
2023-05-03 13:46:12 +0000

    Menu fully functioning

--- PizzaPalace/menu/views.py
+++ PizzaPalace/menu/views.py
@@ -1,71 +1,14 @@
 from django.shortcuts import render
-seed = [
-    {
-        "name": "Margharita",
-        "ingredients": "Cheese, Sauce",
-        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
-        "tags": [
-            "vegetarian"
-        ],
-        "price": {
-            "small": "1.800kr",
-            "medium": "1.800kr",
-            "large": "1.800kr"
-        }
-    },
-    {
-        "name": "Margharita",
-        "ingredients": "Cheese, Sauce",
-        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
-        "tags": [
-            "vegetarian"
-        ],
-        "price": {
-            "small": "1.800kr",
-            "medium": "1.800kr",
-            "large": "1.800kr"
-        }
-    },
-    {
-        "name": "Margharita",
-        "ingredients": "Cheese, Sauce",
-        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
-        "tags": [
-            "vegetarian"
-        ],
-        "price": {
-            "small": "1.800kr",
-            "medium": "1.800kr",
-            "large": "1.800kr"
-        }
-    },
-    {
-        "name": "Hawaiian",
-        "ingredients": "Cheese, Sauce, pineapple",
-        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
-        "tags": [
-            "vegetarian"
-        ],
-        "price": {
-            "small": "1.800kr",
-            "medium": "1.800kr",
-            "large": "1.800kr"
-        }
-    },
-    {
-        "name": "Clayo",
-        "ingredients": "Clay, Mayo",
-        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
-        "tags": [
-            "vegetarian"
-        ],
-        "price": {
-            "small": "1.800kr",
-            "medium": "1.800kr",
-            "large": "1.800kr"
-        }
-    }
-]
+from pizza.models import HasP 
 
 def main(request):
-    return render(request, "menu.html", context={ "seed": seed })
+    context = HasP.objects.all().select_related('PID').select_related('TID')
+    lis = {}
+    for object in context:
+        if object.PID.name in lis:
+            lis[object.PID.name]["toppings"] += ", " + object.TID.name
+        else:
+            lis[object.PID.name] = {"name": object.PID.name, "toppings": object.TID.name, "pricesmall": object.PID.pricesmall, "pricemedium": object.PID.pricemedium, "pricelarge": object.PID.pricelarge, "pic": object.PID.pic}
+    newlis = {"pizzas": lis.values()}
+
+    return render(request, "menu.html", newlis)
