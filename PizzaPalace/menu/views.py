ea971dc864b8ceed1fb3dd77dafe600712fdcb4a
AgustM22 <agust22@ru.is>
2023-05-03 11:42:23 +0000

    merge

--- PizzaPalace/menu/views.py
+++ PizzaPalace/menu/views.py
@@ -1,5 +1,71 @@
 from django.shortcuts import render
+seed = [
+    {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": {
+            "small": "1.800kr",
+            "medium": "1.800kr",
+            "large": "1.800kr"
+        }
+    },
+    {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": {
+            "small": "1.800kr",
+            "medium": "1.800kr",
+            "large": "1.800kr"
+        }
+    },
+    {
+        "name": "Margharita",
+        "ingredients": "Cheese, Sauce",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": {
+            "small": "1.800kr",
+            "medium": "1.800kr",
+            "large": "1.800kr"
+        }
+    },
+    {
+        "name": "Hawaiian",
+        "ingredients": "Cheese, Sauce, pineapple",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": {
+            "small": "1.800kr",
+            "medium": "1.800kr",
+            "large": "1.800kr"
+        }
+    },
+    {
+        "name": "Clayo",
+        "ingredients": "Clay, Mayo",
+        "picture": "https://media.istockphoto.com/id/1280329631/photo/italian-pizza-margherita-with-tomatoes-and-mozzarella-cheese-on-wooden-cutting-board-close-up.jpg?b=1&s=170667a&w=0&k=20&c=_t83ocY59IayPnspluN99xOM_RQ5ytAMTfXQperbL_I=",
+        "tags": [
+            "vegetarian"
+        ],
+        "price": {
+            "small": "1.800kr",
+            "medium": "1.800kr",
+            "large": "1.800kr"
+        }
+    }
+]
 
-# Create your views here.
 def main(request):
-    return render(request, "menu.html")
+    return render(request, "menu.html", context={ "seed": seed })
