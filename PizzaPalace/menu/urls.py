52892ddf517759db86a14ef55cbda5a705064353
AgustM22 <agust22@ru.is>
2023-05-05 15:04:50 +0000

    starting pizzaview

--- PizzaPalace/menu/urls.py
+++ PizzaPalace/menu/urls.py
@@ -4,5 +4,6 @@ from . import views
 # Localhost:8000/login
 urlpatterns = [
     path("", views.main, name="menu"),
-    path("filter", views.filter, name="filter")
+    path("filter", views.filter, name="filter"),
+    path("<int:pizza_id>", views.pizzaview, name="pizzaview")
 ]
--- PizzaPalace/menu/views.py
+++ PizzaPalace/menu/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from pizza.models import HasP 
+from pizza.models import HasP, Product, Topping
 
 def main(request):
     context = HasP.objects.all().select_related('PID').select_related('TID')
-    PizzaDict = sort_data(context)
+    PizzaDict = sort_data(context, False)
 
     return render(request, "menu.html", PizzaDict)
 
@@ -12,19 +12,38 @@ def filter(request):
     if request.method == 'GET':
         filter = request.GET['filter']
         context = HasP.objects.filter().select_related('PID').select_related('TID')
-        PizzaDict = sort_data(context)
+        PizzaDict = sort_data(context, False)
         EditedPizzaDict = []
         for pizza in PizzaDict["pizzas"]:
             if pizza["name"].lower().find(filter.lower()) != -1:
                 EditedPizzaDict.append(pizza)
         return JsonResponse(EditedPizzaDict, safe=False)
 
-def sort_data(context):
+def sort_data(context, view):
     lis = {}
+    item_name = None
     for object in context:
         if object.PID.name in lis:
             lis[object.PID.name]["toppings"] += ", " + object.TID.name
         else:
-            lis[object.PID.name] = {"id": object.id, "name": object.PID.name, "toppings": object.TID.name, "pricesmall": object.PID.pricesmall, "pricemedium": object.PID.pricemedium, "pricelarge": object.PID.pricelarge, "pic": object.PID.pic}
-    PizzaDict = {"pizzas": lis.values()}
-    return PizzaDict
+            lis[object.PID.name] = {"id": object.PID.id, "description": object.PID.description, "name": object.PID.name, "toppings": object.TID.name, "pricesmall": object.PID.pricesmall, "pricemedium": object.PID.pricemedium, "pricelarge": object.PID.pricelarge, "pic": object.PID.pic}
+            if view:
+                item_name = object.PID.name
+    if view:
+        value = lis.pop(item_name)
+        return {"pizza": value}
+    else:
+        PizzaDict = {"pizzas": lis.values()}
+        return PizzaDict
+
+def pizzaview(request, pizza_id):
+    context = Product.objects.filter(id=pizza_id)
+    if not context:
+        context = HasP.objects.all().select_related('PID').select_related('TID')
+        PizzaDict = sort_data(context, False)
+        return render(request, "menu.html", PizzaDict)
+    else:
+        context = HasP.objects.all().select_related('PID').select_related('TID').filter(PID=pizza_id)
+        PizzaDict = sort_data(context, True)
+        PizzaDict["toppings"] = Topping.objects.all()
+        return render(request, "pizzaview.html", PizzaDict)
