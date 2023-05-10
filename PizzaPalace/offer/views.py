8a9a8fd4f6b7daf87f07cc1d3c2f53a2a5cd2b70
AgustM22 <agust22@ru.is>
2023-05-10 09:53:43 +0000

    finished offer and menu

--- PizzaPalace/offer/views.py
+++ PizzaPalace/offer/views.py
@@ -3,19 +3,37 @@ from django.http import JsonResponse
 from pizza.models import Offer, Product
 
 def main(request):
-    context = {'offers': Offer.objects.all()}
-    return render(request, "offer.html", context)
+    context = Offer.objects.all()
+    sort = "1"
+    Offerlis = create_offer_list(context, sort)
+
+    return render(request, "offer.html", {"offers": Offerlis})
 
 def filter(request):
     if request.method == 'GET':
         filter = request.GET['filter']
+        sort = request.GET['sort']
         context = Offer.objects.filter(name__icontains=filter)
-        Offerlis = []
-        for object in context:
-            templis = {"id": object.id, "name": object.name, "description": object.description, "price": object.price, "pic": object.pic}
-            Offerlis.append(templis)
+        Offerlis = create_offer_list(context, sort)
 
         return JsonResponse(Offerlis, safe=False)
+
+def create_offer_list(context, sort):
+    Offerlis = []
+    for object in context:
+        templis = {"id": object.id, "name": object.name, "description": object.description, "price": object.price, "pic": object.pic}
+        Offerlis.append(templis)
+    
+    if sort == "1":
+        Offerlis = sorted(Offerlis, key=lambda x: x['name'])
+    if sort == "2":
+        Offerlis = sorted(Offerlis, key=lambda x: x['name'], reverse=True)
+    if sort == "3":
+        Offerlis = sorted(Offerlis, key=lambda x: x['price'])
+    if sort == "4":
+        Offerlis = sorted(Offerlis, key=lambda x: x['price'], reverse=True)
+
+    return Offerlis
 
 def offerview(request, offer_id):
     context = Offer.objects.filter(id=offer_id)
